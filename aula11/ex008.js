832c8a0933415a49775423a2be38cb2bd858c8fd
Eric Ambiel <eric.ambiel@gmail.com>
2019-06-09 10:13:20 -0300

    Termino aula 11 - Condições Parte 1

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel =61
+
+console.log(`A Velocidade do seu carro é ${vel} Km/h`)
+
+if (vel > 60){
+    console.log(`Ultrapassou a velocidade maxima permitida`)
+}
+
+console.log(`Use o sinto de segurança!!!`)
