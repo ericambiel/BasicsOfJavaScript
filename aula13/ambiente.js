22a8f1517ae97581c4197f4e67b09116756be3b2
Eric Ambiel <eric.ambiel@gmail.com>
2019-06-17 17:30:01 -0300

    Reorganizado arquivos, termino aula 13 Repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+ var c = 0
+ 
+ do {
+    console.log(`Passo ${c}`)
+    // c = c + 1
+    c++
+ } while (c < 500)
+
+/*
+ while (c < 500) {
+    console.log(`Passo ${c}`)
+    // c = c + 1
+    c++
+ }
+*/
