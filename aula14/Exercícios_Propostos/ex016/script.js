bf8a63f9dfc08d99da7b6ccc14eac9581dc2f029
Eric Ambiel <eric.ambiel@gmail.com>
2019-06-18 20:29:42 -0300

    Aula 14 Ex16 terminado

---
+++ aula14/Exercícios_Propostos/ex016/script.js
@@ -0,0 +1,30 @@
+function calcular() {
+    var passo = Number(window.document.querySelector('input#txtpasso').value)
+    var fim = Number(window.document.querySelector('input#txtfim').value)
+    var inicio = Number(window.document.querySelector('input#txtinicio').value)
+
+    if (inicio > fim) {
+        alert('[ERRO 01] - Início não pode ser maior doque o fim!')
+        return
+    } else if (passo <= 0 || fim <=0) {
+        alert('[ERRO 02] - Passo ou fim não podem vazios ou menor ou igual a 0!')
+        return
+    } else if (fim > 10000 || inicio < -10000) {
+        alert('[ERRO 03] - Os valores de inicio e fim não podem ultrapassar -10000 ou 10000.')
+        return
+    }
+    /*Limpa o elemento para que se a função for executada novamente 
+        não some com o resultado anterior.*/
+    window.document.getElementById ('res').innerHTML = null
+    
+    //Variavel recebe objeto res de modelo.html
+    resultado = window.document.getElementById ('res')
+
+    for (i=inicio,somatoria=inicio;i<=fim;i=i+passo) {
+        resultado.innerHTML += String(somatoria) + " &#x1F449 "
+        somatoria = somatoria + passo 
+    }
+    /*Hash do Emoji Codepoint, substitua U+ por &#x no Codepoint, 
+        mais exemplos em "https://emojipedia.org/emoji/" */
+    resultado.innerHTML += " &#x1F3C1 "
+}
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('INICIO')
+for (var i = 1;i <= 10;i++) {
+    console.log(i)
+}
+console.log('FIM')
