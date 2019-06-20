42feb41bb1fd92f7a2e7f48e740d220d32878e19
Eric Ambiel <eric.ambiel@gmail.com>
2019-06-19 21:31:04 -0300

    Terminado exe 17 e modificado ex16

--- aula14/Exercícios_Propostos/ex016/script.js
+++ aula14/Exercícios_Propostos/ex016/script.js
@@ -1,32 +1,44 @@
 function calcular() {
-    var passo = Number(window.document.querySelector('input#txtpasso').value)
-    var fim = Number(window.document.querySelector('input#txtfim').value)
-    var inicio = Number(window.document.querySelector('input#txtinicio').value)
-
+    //let, variavel só existe durante a execução dessa função
+    let passo = window.document.querySelector('input#txtpasso').valueAsNumber
+    let fim = window.document.querySelector('input#txtfim').valueAsNumber
+    let inicio = window.document.querySelector('input#txtinicio').valueAsNumber
+    let resultado = window.document.getElementById ('res')
+    
     //Validação dos dados de entrada
-    if (inicio > fim) {
-        alert('[ERRO 01] - Início não pode ser maior doque o fim!')
-        return
-    } else if (passo <= 0 || fim <=0) {
-        alert('[ERRO 02] - Passo ou fim não podem vazios ou menor ou igual a 0!')
+    if (passo <= 0 || fim <=0) {
+        alert('[ERRO 02] - Passo ou fim não podem ser menor ou igual a 0!')
         return
     } else if (fim > 10000 || inicio < -10000) {
         alert('[ERRO 03] - Os valores de inicio e fim não podem ultrapassar -10000 ou 10000.')
         return
-    }
-    /*Limpa o elemento para que se a função for executada novamente 
-        não some com o resultado anterior.*/
-    window.document.getElementById ('res').innerHTML = null
-    
-    //Variavel recebe objeto res de modelo.html
-    resultado = window.document.getElementById ('res')
+    } else if (window.document.querySelector('input#txtpasso').value.length == 0 ||
+                window.document.querySelector('input#txtfim').value.length == 0 ||
+                window.document.querySelector('input#txtinicio').value.length == 0) {
+                    alert('[ERRO 04] - Campos não podem ser vazios')
+                    return
+    } else {
+        if (inicio < fim) { // Contagem crescente
+            /*Limpa o elemento para que se a função for executada novamente 
+            não some com o resultado anterior.*/
+            resultado.innerHTML = null
+            
+            //Popula elemento res em modelo.html com o resultado
+            for (let i=inicio,somatoria=inicio;i<=fim;i+=passo) {
+                resultado.innerHTML += String(i) + " &#x1F449 "
 
-    //Popula elemento res em modelo.html com o resultado
-    for (i=inicio,somatoria=inicio;i<=fim;i=i+passo) {
-        resultado.innerHTML += String(somatoria) + " &#x1F449 "
-        somatoria = somatoria + passo 
+            }
+        } else { // contagem decrescente
+            resultado.innerHTML = null
+            
+            for (let i=inicio,somatoria=inicio;i>=fim;i-=passo) {
+                resultado.innerHTML += String(i) + " &#x1F449 "
+            }
+        }
+
     }
-    /*Hash do Emoji Codepoint, substitua U+ por &#x no Codepoint, 
+    
+    /*Hash do Emoji Codepoint, substitua U+Coldpoint por &#xCodepoint ou \u{Codepoint} no Codepoint, 
         mais exemplos em "https://emojipedia.org/emoji/" */
     resultado.innerHTML += " &#x1F3C1 "
 }
--- aula14/Exercícios_Propostos/ex017/script.js
+++ aula14/Exercícios_Propostos/ex017/script.js
@@ -1,1 +1,14 @@
-//alert('Olá')
+function calcular() {
+    var multiplicador = document.querySelector('input#inpmult').valueAsNumber
+        
+    if (document.querySelector('input#inpmult').value == "") {
+        window.alert('Multilicador não pode ser vazio')
+        return
+    }
+
+    document.getElementById('tabuada').options[0].text = null
+
+    for (var i=0; i <= 10; i++) {
+        document.getElementById('tabuada').options[i].text = `${multiplicador} x ${i+1} = ` + multiplicador*(i+1)
+    }
+}
